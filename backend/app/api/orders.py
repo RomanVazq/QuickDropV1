af6437007615f25b1dbba475badff133f3936720
Roman Ramos <romanrv1217@gmail.com>
2026-01-06 17:07:25 -0600

    import body

--- backend/app/api/orders.py
+++ backend/app/api/orders.py
@@ -1,7 +1,7 @@
 import uuid
 from datetime import datetime
 from typing import List, Optional
-from fastapi import APIRouter, Depends, HTTPException, status, Query
+from fastapi import APIRouter, Depends, HTTPException, status, Query, Body
 from sqlalchemy.orm import Session
 from pydantic import BaseModel, ConfigDict
 
